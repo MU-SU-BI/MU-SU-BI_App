c5ce036ec0c1a9bdf4c527f66e64ad056152c459
iamdudumon <kksy4511@naver.com>
2024-07-18 16:28:17 +0900

    refactor: mvp 패턴 채택

--- app/src/main/java/com/example/musubi/view/activity/MainActivity.java
+++ app/src/main/java/com/example/musubi/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.musubi;
+package com.example.musubi.view.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.musubi.R;
 
 public class MainActivity extends AppCompatActivity {
 
