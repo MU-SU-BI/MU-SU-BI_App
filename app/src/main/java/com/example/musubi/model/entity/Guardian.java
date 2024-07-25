1ac89ad1b2a40b164c7cedf7a38433348bfda9da
iamdudumon <kksy4511@naver.com>
2024-07-25 18:35:32 +0900

    bb

--- app/src/main/java/com/example/musubi/model/entity/Guardian.java
+++ app/src/main/java/com/example/musubi/model/entity/Guardian.java
@@ -3,7 +3,7 @@ package com.example.musubi.model.entity;
 public class Guardian extends Person{
     private final Person user;
 
-    protected Guardian(long id, String email, String name, int age, String nickname, String phone, String address, Gender gender, Person user) {
+    protected Guardian(long id, String email, String name, Gender gender, int age, String nickname, String phone, String address, Person user) {
         super(id, email, name, gender, age, nickname, phone, address);
         this.user = user;
     }
