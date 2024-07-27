8fee0b7eda0558aa9726e8d7a1c1cdeae6509522
iamdudumon <kksy4511@naver.com>
2024-07-28 02:40:03 +0900

    feat: 커스텀 버튼을 유지하기 위해 SPF에 저장

--- app/src/main/java/com/example/musubi/view/fragment/CallFragment.java
+++ app/src/main/java/com/example/musubi/view/fragment/CallFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.example.musubi.R;
+import com.example.musubi.model.local.SPFManager;
 import com.example.musubi.presenter.contract.CallContract;
 import com.example.musubi.presenter.implementation.CallPresenter;
 import com.example.musubi.util.callback.OnActionEnrollListener;
@@ -18,11 +21,13 @@ import com.example.musubi.view.dialog.CustomActionDialog;
 import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class CallFragment extends Fragment implements CallContract.View, OnActionEnrollListener {
     View view;
     CallPresenter presenter;
 
+    RelativeLayout layout;
     Button button1;
     Button button2;
     Button button3;
@@ -30,16 +35,18 @@ public class CallFragment extends Fragment implements CallContract.View, OnActio
     Button button5;
     Button addCustomActionButton;
 
+    SPFManager spfManager;
     ArrayList<Integer> buttonIdArray;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.view = view;
-        initView();
         presenter = new CallPresenter(this);
-        buttonIdArray = new ArrayList<>();
-        buttonIdArray.add(button5.getId());
+        spfManager = new SPFManager(requireContext(), "CallFragment");
+        initView();
+        initButtonList();
+        spfManager.getEditor().clear();
     }
 
     @Nullable
@@ -49,6 +56,7 @@ public class CallFragment extends Fragment implements CallContract.View, OnActio
     }
 
     private void initView() {
+        layout  = this.view.findViewById(R.id.main);
         button1 = this.view.findViewById(R.id.button1);
         button2 = this.view.findViewById(R.id.button2);
         button3 = this.view.findViewById(R.id.button3);
@@ -64,6 +72,42 @@ public class CallFragment extends Fragment implements CallContract.View, OnActio
         });
     }
 
+    private void createNewButton(String actionName) {
+        MaterialButton newButton = new MaterialButton(requireContext());
+
+        // new 버튼 스타일 추가
+        newButton.setText(actionName);
+        newButton.setId(View.generateViewId());
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.addRule(RelativeLayout.BELOW, buttonIdArray.get(buttonIdArray.size() - 1)); // 가장 마지막 버튼 아래에 위치
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL); // 가운데 정렬
+        params.setMargins(0, 16, 0, 16); // 마진 설정
+        layout.addView(newButton, params);
+        buttonIdArray.add(newButton.getId());
+    }
+
+    private void updateAddButtonPosition(int lasButtonId) {         // 기존의 add 버튼 위치 변경
+        RelativeLayout.LayoutParams addButtonParams = (RelativeLayout.LayoutParams) addCustomActionButton.getLayoutParams();
+        addButtonParams.addRule(RelativeLayout.BELOW, lasButtonId); // newButton 아래에 위치
+        addCustomActionButton.setLayoutParams(addButtonParams);
+    }
+
+    private void initButtonList() {
+        buttonIdArray  = new ArrayList<>();
+        buttonIdArray.add(button5.getId());
+
+        while (true) {
+            String newBtnActionName = spfManager.getSharedPreferences().getString("newBtn" + buttonIdArray.size(), null);
+            if (newBtnActionName == null)
+                break ;
+            createNewButton(newBtnActionName);
+        }
+        updateAddButtonPosition(buttonIdArray.get(buttonIdArray.size() - 1));
+    }
+
     @Override
     public void onCallSuccess(String message) {
 
@@ -76,25 +120,8 @@ public class CallFragment extends Fragment implements CallContract.View, OnActio
 
     @Override
     public void onActionEnroll(String actionName) {
-        RelativeLayout layout = this.view.findViewById(R.id.main);
-        MaterialButton newButton = new MaterialButton(requireContext());
-
-        // new 버튼 스타일 추가
-        newButton.setText(actionName);
-        newButton.setId(View.generateViewId());
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-        params.addRule(RelativeLayout.BELOW, buttonIdArray.get(buttonIdArray.size() - 1)); // button5 아래에 위치
-        params.addRule(RelativeLayout.CENTER_HORIZONTAL); // 가운데 정렬
-        params.setMargins(0, 16, 0, 16); // 마진 설정
-        layout.addView(newButton, params);
-
-        // 기존의 add 버튼 위치 변경
-        RelativeLayout.LayoutParams addButtonParams = (RelativeLayout.LayoutParams) addCustomActionButton.getLayoutParams();
-        addButtonParams.addRule(RelativeLayout.BELOW, newButton.getId()); // newButton 아래에 위치
-        addCustomActionButton.setLayoutParams(addButtonParams);
-        buttonIdArray.add(newButton.getId());
+        createNewButton(actionName);
+        updateAddButtonPosition(buttonIdArray.get(buttonIdArray.size() - 1));
+        spfManager.getEditor().putString("newBtn" + (buttonIdArray.size() - 1), actionName).apply();
     }
 }
